12f6c35f08d0bad2dafd498d21c2c1ee6915c153
JohnathanH04 <huang.johnny23@gmail.com>
2025-09-23 15:24:11 -0400

    hw1

---
+++ proj1/base/start.h
@@ -0,0 +1,12 @@
+#ifndef start_H
+#define start_H
+#define LOG(argument) std::cout << argument << '\n'
+
+#include "raylib.h"
+#include <stdio.h>
+
+enum AppStatus { TERMINATED, RUNNING };
+
+Color ColorFromHex(const char *hex);
+
+#endif // CS3113_H
